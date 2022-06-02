25e8c30ca0b44ece79ec3dd09aa9f51daae5ce57
Nusair Haq <nhaq@splunk.com>
2022-06-02 19:59:08 -0400

    added homepage images

--- packages/sui-visualizations-example/src/main/webapp/pages/start/index.jsx
+++ packages/sui-visualizations-example/src/main/webapp/pages/start/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import layout from '@splunk/react-page';
-import RadarChart from '@splunk/radar-chart';
+// import RadarChart from '@splunk/radar-chart';
+import VisualizationCard from '@splunk/visualization-card';
 import { SplunkThemeProvider } from '@splunk/themes';
-
 import { defaultTheme, getThemeOptions } from '@splunk/splunk-utils/themes';
+
+import Sankey from './Images/sankey.png';
+import Radar from './Images/radar.png';
+import Treemap from './Images/treemap.png';
 
 import { StyledContainer, StyledGreeting } from './StartStyles';
 
@@ -13,9 +17,47 @@ const themeProviderSettings = getThemeOptions(defaultTheme() || 'enterprise');
 layout(
     <SplunkThemeProvider {...themeProviderSettings}>
         <StyledContainer>
-            <StyledGreeting>Hello, from inside SuiVisualizationsExample!</StyledGreeting>
-            <div>Your component will appear below.</div>
-            <RadarChart name="from inside RadarChart" />
+            <StyledGreeting>Visualizations with Splunk UI Toolkit</StyledGreeting>
+            <div>
+                This app is designed to showcase and provide practical code examples of how
+                visualizations can be done with the Splunk UI Toolkit. There are many options
+                available to Splunk developers on how they want to visualize data outside of Splunk
+                Dashboard Studio or Simple XML Dashboards. The following cards direct to examples of
+                three possible ways a developer can directly code visualizations in their Splunk
+                app.
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                    marginTop: '25px',
+                }}
+            >
+                <div>
+                    <VisualizationCard
+                        src={Sankey}
+                        title="Splunk Visualization (Sankey Diagram)"
+                        subtitle="Without Dashboard Framework (ReactJS Only)"
+                        alt="Sankey Diagram Image"
+                    />
+                </div>
+                <div>
+                    <VisualizationCard
+                        src={Radar}
+                        title="3rd Party Visualization (Radar Chart)"
+                        subtitle="Within Splunk Dashboard Framework"
+                        alt="Radar Chart Image"
+                    />
+                </div>
+                <div>
+                    <VisualizationCard
+                        src={Treemap}
+                        title="3rd Party Visualization (Treemap Diagram)"
+                        subtitle="Without Dashboard Framework (ReactJS Only)"
+                        alt="Treemap Diagram Image"
+                    />
+                </div>
+            </div>
         </StyledContainer>
     </SplunkThemeProvider>
 );
--- packages/visualization-card/src/VisualizationCard.jsx
+++ packages/visualization-card/src/VisualizationCard.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import Card from '@splunk/react-ui/Card';
-import Sankey from './Images/sankey.png';
 
-class VisualizationCard = (props) => {
-    static propTypes = {
-        name: PropTypes.string,
-    };
-
-    const style = { width: 300, height: 400, margin: '0 20px 20px 0' };
+const VisualizationCard = (props) => {
     return (
-        <div>
-            <Card style={style}>
-                <Card.Body
-                    style={{
-                        background: '#d9edf7',
-                        textAlign: 'center',
-                        padding: '84px 50px',
-                        color: '#3a87ad',
-                    }}
-                >
-                    <img className="thumbnail-image" src={props.image} alt={`${props.image}`} />
-                </Card.Body>
-                <Card.Header title="Blue" subtitle="Check Mark" />
-            </Card>
-        </div>
+        <Card>
+            <img src={props.src} style={{ width: '400px', height: '400px' }} alt={props.alt} />
+            <Card.Header title={props.title} subtitle={props.subtitle} />
+        </Card>
     );
 };
 
