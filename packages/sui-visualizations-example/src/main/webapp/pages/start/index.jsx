a5eb69a02c42fcfa0bc477f3deaab28f2e791038
Nusair Haq <nhaq@splunk.com>
2022-06-23 18:28:15 -0400

    added react router and finalized data

--- packages/sui-visualizations-example/src/main/webapp/pages/start/index.jsx
+++ packages/sui-visualizations-example/src/main/webapp/pages/start/index.jsx
@@ -1,62 +1,32 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import layout from '@splunk/react-page';
 // import RadarChart from '@splunk/radar-chart';
-import VisualizationCard from '@splunk/visualization-card';
-import { SplunkThemeProvider } from '@splunk/themes';
 
-import { StyledContainer, StyledGreeting } from './StartStyles';
+import Homepage from './Homepage';
+import SplunkVizExample from '../splunkViz/SplunkViz';
+import ThirdPartyExample from '../3PWithoutDF/3pWithoutDF';
+import RadarDashboard from '../3PwithDF/RadarDashboard';
 
-const themeToVariant = {
-    enterprise: { colorScheme: 'light', family: 'enterprise' },
-};
 layout(
-    <SplunkThemeProvider {...themeToVariant.enterprise}>
-        <StyledContainer>
-            <StyledGreeting>Visualizations with Splunk UI Toolkit</StyledGreeting>
-            <div>
-                This app is designed to showcase and provide practical code examples of how
-                visualizations can be done with the Splunk UI Toolkit. There are many options
-                available to Splunk developers on how they want to visualize data outside of Splunk
-                Dashboard Studio or Simple XML Dashboards. The following cards direct to examples of
-                three possible ways a developer can directly code visualizations in their Splunk
-                app. Visit each page to learn more about that method&apos;s implementation and the
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-evenly',
-                    marginTop: '25px',
-                }}
-            >
-                <div>
-                    <VisualizationCard
-                        src="/static/app/sui-visualizations-example/sankey.png"
-                        title="Splunk Visualization (Sankey Diagram)"
-                        subtitle="Without Dashboard Framework (ReactJS Only)"
-                        alt="4"
-                        url="/app/sui-visualizations-example/splunkViz"
-                    />
-                </div>
-                <div>
-                    <VisualizationCard
-                        src="/static/app/sui-visualizations-example/radar.png"
-                        title="3rd Party Visualization (Radar Chart)"
-                        subtitle="Within Splunk Dashboard Framework"
-                        alt="Radar Chart Image"
-                        url="/app/sui-visualizations-example/3PwithDF"
-                    />
-                </div>
-                <div>
-                    <VisualizationCard
-                        src="/static/app/sui-visualizations-example/funnel.png"
-                        title="3rd Party Visualization (Funnel Chart)"
-                        subtitle="Without Dashboard Framework (ReactJS Only)"
-                        alt="Treemap Diagram Image"
-                        url="/app/sui-visualizations-example/3PwithoutDF"
-                    />
-                </div>
-            </div>
-        </StyledContainer>
-    </SplunkThemeProvider>
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="en-US/app/sui-visualizations-example/start" element={<Homepage />} />
+                <Route
+                    path="/app/sui-visualizations-example/splunkViz1"
+                    element={<SplunkVizExample />}
+                />
+                <Route
+                    path="/app/sui-visualizations-example/3PwithDF1"
+                    element={<RadarDashboard />}
+                />
+                <Route
+                    path="/app/sui-visualizations-example/3PwithoutDF1"
+                    element={<ThirdPartyExample />}
+                />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 );
